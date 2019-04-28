5272eef82e721e6a2c0ea0b50d9a5c4ae42af46f
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2019-04-28 11:37:41 +0200

    Added method to get area of roof to roofBuilder

--- src/main/java/FunctionLayer/RoofBuilder.java
+++ src/main/java/FunctionLayer/RoofBuilder.java
@@ -11,4 +11,16 @@ package FunctionLayer;
  */
 public class RoofBuilder {
     
+    public double getRoofArea(int carportLength, int carportWidth, int roofAngle){
+        //+15 pga 15cm tagudhæng i hver side af carporten
+        int katete = carportWidth/2+15;
+        double hypotenuse = katete/Math.cos(Math.toRadians(roofAngle));
+        double roofArea = carportLength*hypotenuse*2;
+        return roofArea;
+    }
+    
+    public static void main(String[] args) {
+        RoofBuilder rb = new RoofBuilder();
+        System.out.println(rb.getRoofArea(1, 22, 42));
+    }
 }
